e6e4216136c0d0d653173e8b3a4888c4f556dc9d
Java-via <offer2016@163.com>
2016-12-07 11:16:32 +0800

    Update z_db_conf.py

--- z_db_conf.py
+++ z_db_conf.py
@@ -4,6 +4,6 @@ import pymysql
 
 
 def db_conf():
-    conn = pymysql.connect(host="54.169.79.230", db="db_doumovies", user="appuser", passwd="123", charset="utf8")
+    conn = pymysql.connect(host="localhost", db="db_doumovies", user="appuser", passwd="123", charset="utf8")
     cur = conn.cursor()
     return conn, cur
